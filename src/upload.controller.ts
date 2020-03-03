ac9e41760e416edb15ae3515d5b8a5338348ccac
Jacky Zheng <jzheng1126@yahoo.com>
2020-03-02 16:55:24 -0800

    Completed file upload and retrieval of all file names. Certain operations are synchronous for now, will update in future.

--- src/upload.controller.ts
+++ src/upload.controller.ts
@@ -1,22 +1,47 @@
 import { Controller, Get, Post, Req, UseInterceptors, UploadedFile, Param, Res } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { readdirSync, existsSync, mkdirSync } from 'fs';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller('upload')
 export class UploadController {
     @Post(':id')
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            // Sync methods not performance optimal
+            destination: function (req, file, callback) {
+                let path = './files/' + req.params.id;
+                !existsSync(path) && mkdirSync(path)
+                callback(null, path)
+            },
+            filename: function (req, file, callback) {
+                callback(null, file.originalname)
+            }
+        })
+    }))
     async uploadFile(@Param('id') userID, @UploadedFile() file) {
         const response = {
             originalname: file.originalname,
             filename: file.filename
         };
-        return response    
+        return response
     }
 
-    @Get(':filepath')
-    getUploadedFile(@Param('filepath') musicFile, @Res() res) {
-        return res.sendFile(musicFile, { root: './files' });
+    @Get(':id')
+    async getAllFiles(@Param('id') userID) {
+        let path = './files/' + userID
+        if (!existsSync(path)) {
+            return {
+                files: []
+            }
+        }
+        else {
+            let directory = readdirSync(path)
+            return {
+                files: directory
+            }
+        }      
     }
 }
